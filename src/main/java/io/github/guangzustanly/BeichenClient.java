00db350596d10f0391b962600d84ef3d857d3cea
zhengxgen <zhengxgen@163.com>
2024-07-01 11:33:44 +0800

    Initial commit

---
+++ src/main/java/io/github/guangzustanly/BeichenClient.java
@@ -0,0 +1,10 @@
+package io.github.guangzustanly;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeichenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
